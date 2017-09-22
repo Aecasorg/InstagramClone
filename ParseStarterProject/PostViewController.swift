bd25446b5bae293fe429bc79eb9440596bc7d328
Henrik Gustavii <henrik@gustavii.com>
2017-09-22 08:55:59 +0200

    Added a post image view controller where you post your images with a comment. Includes alert and busy indicator. Found one issue and that was that I could not post images larger than 10MB which I put down as an issue with the free Parse/AWS that I am using. Something that can be investigated further.

--- ParseStarterProject/PostViewController.swift
+++ ParseStarterProject/PostViewController.swift
@@ -7,9 +7,98 @@
 //
 
 import UIKit
+import Parse
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var activityIndicator = UIActivityIndicatorView()
+    
+    @IBOutlet weak var imageToPost: UIImageView!
+    
+    @IBAction func chooseAnImage(_ sender: Any) {
+    
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageToPost.image = image
+            
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func createAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            
+            alert.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBAction func postImage(_ sender: Any) {
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+    
+        let post = PFObject(className: "Posts")
+        
+        post["message"] = messageTextField.text
+        
+        post["userid"] = PFUser.current()?.objectId!
+        
+        let imageData = UIImagePNGRepresentation(imageToPost.image!)
+        
+        let imageFile = PFFile(name: "image.png", data: imageData!)
+        
+        post["imageFile"] = imageFile
+        
+        post.saveInBackground { (success, error) in
+            
+            self.activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if error != nil {
+                
+                self.createAlert(title: "Could not post image", message: "Please try again later")
+                
+            } else {
+                
+                self.createAlert(title: "Image Posted!", message: "You image has been posted successfully")
+                
+                self.messageTextField.text = ""
+                
+                self.imageToPost.image = UIImage(named: "pic-of-man.png")
+                
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
